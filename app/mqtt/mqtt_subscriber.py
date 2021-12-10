7d34d88663fe2a6f6234745ecf9acc72c2569ea4
German Velardez <39204261+Germanvelardez2018@users.noreply.github.com>
2021-12-10 10:59:18 -0300

    Update mqtt_subscriber.py

--- app/mqtt/mqtt_subscriber.py
+++ app/mqtt/mqtt_subscriber.py
@@ -10,37 +10,35 @@ from Credentials import credentials as  DATA
 
 
 _API_NAME_ = "mqtt client"
-
+ 
+#----------------------Callback functions--------------------------
 def on_connect(client,userdata,flags,rc):
-    if rc == 0:
-
-        print("[{}]connected to the broker".format(_API_NAME_))
-        print("[{}] subcribe to the topic {}".format(_API_NAME_,DATA.TOPIC))
-        client.subscribe(DATA.TOPIC)
-    else:
-        print("[{}]Connection Error".format(_API_NAME_))
-
+   if rc == 0:
+       print("[{}]connected to the broker".format(_API_NAME_))
+       print("[{}] subscribe to the topic {}".format(_API_NAME_,TOPIC))
+       client.subscribe("X1111")
+   else:
+       print("[{}]Connection Error".format(_API_NAME_))
+ 
 def on_disconnected(client,userdata,rc):
-
-    if rc !=0:
-        print("[{}]disconnected to the broker".format(_API_NAME_))
-
+   if rc !=0:
+       print("[{}]disconnected to the broker".format(_API_NAME_))
+ 
 def on_message(client,userdata,msg):
-    print("[{}]you have a new message. {}".format(_API_NAME_,str(msg.payload)))
-
-
-
-print("init subscriber")
-
-client = mqtt.Client(client_id= "simo-sub", clean_session=False)
-
-
-print("conencted: {} port: {}".format(DATA.URL,DATA.PORT))
-client.on_connect = on_connect
-client.on_message = on_message
-#client.on_disconnect = on_disconnected
-
-
-client.username_pw_set(DATA.ID,DATA.PASS)
-client.connect(host=DATA.URL,port=DATA.PORT,keepalive=60)
-client.loop_forever()
+   print("[{}]you have a new message. {}".format(_API_NAME_,str(msg.payload)))
+#-----------------------------------------------------------------
+ 
+def Main():
+   print(_API_NAME_)
+   client = mqtt.Client(client_id= "simo-sub", clean_session=False)
+   print("connected: {} port: {}".format(BROKER2,PORT))
+   client.on_connect = on_connect
+   client.on_message = on_message
+   #client.on_disconnect = on_disconnected
+   client.username_pw_set(ID,PASS)
+   client.connect(host=BROKER2,port=PORT,keepalive=60)
+   client.loop_forever()
+ 
+ 
+if __name__ == "__main__":
+   Main()
